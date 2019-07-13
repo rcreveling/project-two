a2ed515da8aaf15fbe764de52cddea777e7085ad
Ryan Creveling <ryancreveling@gmail.com>
2019-07-13 11:11:38 -0400

    Added a few user pages, form validation, still needs work but workable build for backend

--- ryanBranch/server.js
+++ ryanBranch/server.js
@@ -9,6 +9,17 @@ var PORT = process.env.PORT || 3000;
 require("./routes/apiRoutes")
 require("./routes/htmlRoutes")
 app.use(express.static("public"))
-
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/login.html'))
+})
+app.get("/topics", function (req, res) {
+    res.sendfile(path.join(__dirname, 'public/topics.html'))
+})
+app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/signup.html'))
+})
+app.get("/welcome", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/welcome.html'))
+})
 
 app.listen(3000);
