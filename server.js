46649bbfddac7d2be09330291e264d25bfe6472a
Ryan Creveling <ryancreveling@gmail.com>
2019-07-17 22:00:19 -0400

    Added ApiRoutes, saving work on admin page

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const path = require('path');
-const db = require("./models")
+const db = require("./models");
 var PORT = process.env.PORT || 3000;
-app.use(express.static("public"))
-const routes = require('./routes/htmlRoutes')
-require("./routes/apiRoutes")
+app.use(express.static("public"));
+const routes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
 
 
 routes(app, path);
+apiRoutes(app, path);
 
 var syncOptions = { force: false };
 
