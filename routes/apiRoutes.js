5b53bef5dd1308b1fb6422ee6f7e2c59f39f6886
Ryan Creveling <ryancreveling@gmail.com>
2019-07-18 18:35:25 -0400

    added ApiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,18 @@ module.exports = function (app) {
         });
     });
 
+    // Get route for retrieving a single post
+    app.get("/api/activities/:id", function (req, res) {
+        db.Activity.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (dbActivity) {
+                res.json(dbActivity);
+            });
+    });
+
     // Create a new example
     app.post("/api/activities/", function (req, res) {
         db.Activity.create(req.body).then(function (dbActivity) {
@@ -21,4 +33,18 @@ module.exports = function (app) {
             res.json(dbActivity);
         });
     });
+
+    // PUT route for updating posts
+    app.put("/api/activities", function (req, res) {
+        db.Activity.update(req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then(function (dbActivity) {
+                res.json(dbActivity);
+            });
+    });
+
 };
