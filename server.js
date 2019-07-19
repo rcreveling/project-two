b2d1d04a7c295204bfeb05e12d12c90a64ca80c3
Ryan Creveling <ryancreveling@gmail.com>
2019-07-18 20:01:20 -0400

    commiting basic re-installations

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const session = require('express-session');
 
 app.use(express.static("public"));
-
+app.use('/semantic', express.static(path.join(__dirname, '/semantic/dist/')))
 // passport related
 require('./config/passport')(passport);
 
@@ -40,13 +40,13 @@ var syncOptions = { force: false }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function () {
-    app.listen(PORT, function () {
-        console.log(
-            "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
+  app.listen(PORT, function () {
+    console.log(
+      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
 
 module.exports = app;
